93f695fb257abb77dbb6996dd86f4e2bd8c0dc37
uvsq21920965 <sarra.belmahdi@gmail.com>
2020-03-03 11:29:10 +0100

    test commit

--- src/test/resources/EmployerTest.java
+++ src/test/resources/EmployerTest.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class EmployerTest {
 	
@@ -7,16 +10,19 @@ public class EmployerTest {
 	
 	
 
-@Test
-public void salaireTest() {
-	
-	double salaireE1=e1.salaire(0);
-	double salaireE2=e2.salaire(2);
-	double salaireE1=e3.salaire(10);
-	
-	assertEquals(salaireE1,1500);
-	assertEquals(salaireE2,1540);
-	assertEquals(salaireE3,1510);
-}
+	@Test
+	public void salaireTest() {
+		
+		double salaireE1=e1.calcule_salaire(0);
+		double salaireE2=e2.calcule_salaire(2);
+		double salaireE3=e3.calcule_salaire(10);
+		System.out.println(salaireE1);
+		System.out.println(salaireE2);
+		System.out.println(salaireE3);
+		
+		assertEquals(salaireE1,1500.0,0);
+		assertEquals(salaireE2,1540.0,0);
+		assertEquals(salaireE3,1700.0,0);
+	}
 
 }
--- src/test/resources/VendeurTest.java
+++ src/test/resources/VendeurTest.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class VendeurTest {
 	
@@ -5,17 +8,16 @@ public class VendeurTest {
 	Vendeur v2=new Vendeur(120);
 	Vendeur v3=new Vendeur(0);
 	
-"test methodes"	
-@Test
-public void salaireTest() {
-	
-	double salaireV1=v1.salaire(0);
-	double salaireV2=v2.salaire(2);
-	double salaireV1=v3.salaire(10);
-	
-	assertEquals(salaireV1,1700);
-	assertEquals(salaireV2,1700);
-	assertEquals(salaireV3,1510);
-}
 
+	@Test
+	public void salaireTest() {
+		
+		double salaireV1=v1.calcule_salaire(0);
+		double salaireV2=v2.calcule_salaire(2);
+		double salaireV3=v3.calcule_salaire(10);
+		
+		assertEquals(salaireV1,1700.0,0);
+		assertEquals(salaireV2,1660.0,0);
+		assertEquals(salaireV3,1700.0,0);
+	}
 }
